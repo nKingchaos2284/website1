8ee5b5dc147fd1c320a084cf6d98460824d70fa0
Jose Mauricio Cabrera Elias <jmcabrerae@gmail.com>
2022-09-02 11:28:49 -0700

    changed both files

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-let favColor = prompt("What is you favorite color?");
-console.log(favColor);
 
-function capitalize() {
-    favColorCap = favColor.charAt(0).toUpperCase();
-    favColor = favColorCap + favColor.slice(1);
+function capitalize(word) {
+    let fl = word.charAt(0).toUpperCase()
+   let favColorst = fl + word.slice(1);
+   return favColorst
 }
-
-if (favColor == null) {
+let favColor
+while(favColor) {
     favColor = prompt("Don't have one? Are you color blind? You gotta like something?");
 }
 
+
 function askColor() {
+    favColor = prompt("What is you favorite color?").toLowerCase();
+    let low=capitalize(favColor)
     if (favColor == "pink") {
-        capitalize();
-        document.write(favColor + "? really? really? really? I guessssss");
+
+        document.write(low + "? really? really? really? I guessssss");
     } else if (favColor == "blue" || favColor == "red" || favColor == "black") {
-        capitalize();
-        document.write(favColor + "? I love those too!");
-    } else if (favColor == "silver" || favColor == "orange" || favColor == "purple" || favColor == "white" || favColor == "GOLD") {
-        capitalize();
-        document.write(favColor + "? VERY MICE!");
+    
+        document.write(low + "? I love those too!");
+    } else if (favColor == "silver" || favColor == "orange" || favColor == "purple" || favColor == "white" || favColor == "gold") {
+    
+        document.write(favColor + "? VERY NICE!");
     } else {
-        capitalize();
-        favColor = prompt(favColor + "? That one's not on the list, might've forgot to add that to the database. Try another!");
+
+        alert(favColor + "? That one's not on the list, might've forgot to add that to the database. Try another!");
         askColor();
     }
 }
@@ -33,14 +35,12 @@ function colorRec() {
     let userNumber = prompt("Pick a number between 1-10");
     if (userNumber > 5) {
         userNumber = 0;
-        fruitRec();
+
     }
-    for (let i = 1; i <= userNumber; i++){
-        document.write("<a href="https://www.google.com">
-    <img src="spiderman.jpg" style="height:400px; width:400px"> 
-</a>
-' />");
-    }
+    for (let i = 1; i <= userNumber; i++) {
+        document.write("< img src = 'BW.png' style = 'height:400px; width:400px' >")
+    
+   }
 
     return userNumber;
 }
